30e4355e392e37f76f5669ecc2635263563c7a40
Jacob Carse <jacob.carse@gmail.com>
2017-11-02 12:34:03 +0000

    Fixed building of dataset

--- main.py
+++ main.py
@@ -1,123 +1,97 @@
 import os
 import cv2
-import csv
 import sys
 import scipy.io as io
 from Data import Data
 from Model import Model
+from Active import Active
 from scipy.spatial import distance
-from sklearn.model_selection import train_test_split
 
 
-def build_dataset(in_dir, out_dir, patch_size, steps, test_percentage, validation_percentage):
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    if not os.path.exists(out_dir + '/train'):
-        os.makedirs(out_dir + '/train/miss')
-        os.makedirs(out_dir + '/train/hit')
-    if not os.path.exists(out_dir + '/test'):
-        os.makedirs(out_dir + '/test/miss')
-        os.makedirs(out_dir + '/test/hit')
-    if not os.path.exists(out_dir + '/validation'):
-        os.makedirs(out_dir + '/validation/miss')
-        os.makedirs(out_dir + '/validation/hit')
+def build_dataset(in_dir, out_dir, patch_size, steps, test_percentage):
+    if in_dir[-1] != '/':
+        in_dir += '/'
+    if out_dir[-1] != '/':
+        out_dir += '/'
+
+    if not os.path.exists(out_dir + 'train'):
+        os.makedirs(out_dir + 'train/miss')
+        os.makedirs(out_dir + 'train/hit')
+    if not os.path.exists(out_dir + 'test'):
+        os.makedirs(out_dir + 'test/miss')
+        os.makedirs(out_dir + 'test/hit')
 
     border_size = patch_size // 2
-    train_counter, test_counter, val_counter = 0, 0, 0
+    train_count, test_count = 0, 0
 
-    for img_no in range(1, 101):
-        orginal_image = cv2.imread(in_dir + '/img' + str(img_no) + '/img' + str(img_no) + '.bmp')
-        shape = orginal_image.shape
-        image = cv2.copyMakeBorder(orginal_image, border_size, border_size, border_size, border_size,
-                                   cv2.BORDER_CONSTANT, value=[0, 0, 0])
-        mat = io.loadmat(in_dir + '/img' + str(img_no) + '/img' + str(img_no) + '_detection.mat')['detection']
-
-        patches, labels = [], []
-        for i in range(1, shape[0], steps):
-            for j in range(1, shape[1], steps):
-                min_dist = 2147483647
-                for point in mat:
-                    dist = distance.euclidean(point, (i, j))
-                    if abs(dist) < min_dist:
-                        min_dist = abs(dist)
-                if 20 > min_dist and min_dist > 3:
-                    continue
-                if min_dist >= 20:
-                    patches.append(image[i:i + patch_size, j:j + patch_size])
-                    labels.append(0)
+    for img_no in range(1, len(os.listdir(in_dir)) + 1):
+        original_image = cv2.imread(in_dir + 'img' + str(img_no) + '/img' + str(img_no) + '.bmp')
+        border_image = cv2.copyMakeBorder(original_image, border_size, border_size, border_size, border_size,
+                                          cv2.BORDER_DEFAULT)
+        points = io.loadmat(in_dir + 'img' + str(img_no) + '/img' + str(img_no) + '_detection.mat')['detection']
+        shape = original_image.shape
+        patches_count, test_patches = 0, 10 - (10 * test_percentage)
+        for i in range(0, shape[0], steps):
+            for j in range(0, shape[1], steps):
+                min_dist = 100
+                for point in points:
+                    dist = distance.euclidean((point[0], point[1]), (i, j))
+                    if dist < min_dist:
+                        min_dist = dist
+                if min_dist <= 3:
+                    cv2.rectangle(original_image, (i - border_size, j - border_size),
+                                  (i + patch_size - border_size, j + patch_size - border_size), (255, 0, 0))
+                    label = 'hit/'
+                    cv2.imwrite(out_dir + '/' + str(img_no) + '.bmp', original_image)
+                elif min_dist > 20:
+                    label = 'miss/'
                 else:
-                    cv2.rectangle(image, (i, j), (i + patch_size, j + patch_size), (255, 0, 0))
-                    patches.append(image[i:i + patch_size, j:j + patch_size])
-                    labels.append(1)
-                if len(patches) == 100:
-                    train_x, test_x, train_y, test_y = train_test_split(patches, labels,
-                                                                        test_size=int(100 * test_percentage))
-                    train_x, val_x, train_y, val_y = train_test_split(train_x, train_y,
-                                                                      test_size=int(100 * validation_percentage))
-                    for i in range(len(train_x)):
-                        with open(out_dir + '/train.csv', 'a', newline='') as csvfile:
-                            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                            if train_y[i] == 0:
-                                cv2.imwrite(out_dir + '/train/miss/' + str(train_counter) + '.bmp', train_x[i])
-                                writer.writerow([out_dir + '/train/miss/' + str(train_counter) + '.bmp'] + [train_y[i]])
-                            else:
-                                cv2.imwrite(out_dir + '/train/hit/' + str(train_counter) + '.bmp', train_x[i])
-                                writer.writerow([out_dir + '/train/hit/' + str(train_counter) + '.bmp'] + [train_y[i]])
-                        train_counter += 1
-                    for i in range(len(test_x)):
-                        with open(out_dir + '/test.csv', 'a', newline='') as csvfile:
-                            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                            if test_y[i] == 0:
-                                cv2.imwrite(out_dir + '/test/miss/' + str(test_counter) + '.bmp', test_x[i])
-                                writer.writerow([out_dir + '/test/miss/' + str(test_counter) + '.bmp'] + [test_y[i]])
-                            else:
-                                cv2.imwrite(out_dir + '/test/hit/' + str(test_counter) + '.bmp', test_x[i])
-                                writer.writerow([out_dir + '/test/hit/' + str(test_counter) + '.bmp'] + [test_y[i]])
-                        test_counter += 1
-                    for i in range(len(val_x)):
-                        with open(out_dir + '/validation.csv', 'a', newline='') as csvfile:
-                            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                            if val_y[i] == 0:
-                                cv2.imwrite(out_dir + '/validation/miss/' + str(val_counter) + '.bmp', val_x[i])
-                                writer.writerow([out_dir + '/validation/miss/' + str(val_counter) + '.bmp'] + [val_y[i]])
-                            else:
-                                cv2.imwrite(out_dir + '/validation/hit/' + str(val_counter) + '.bmp', val_x[i])
-                                writer.writerow([out_dir + '/validation/hit/' + str(val_counter) + '.bmp'] + [val_y[i]])
+                    continue
+                if patches_count > test_patches:
+                    thing = 'test/'
+                    test_count += 1
+                else:
+                    thing = 'train/'
+                    train_count += 1
+                patch = border_image[j:j + patch_size, i: i + patch_size]
+                cv2.imwrite(out_dir + thing + label + str(img_no) + '_' + str(i) + ',' + str(j) + '.bmp', patch)
 
-                        val_counter += 1
-                    patches, labels = [], []
-                    if (train_counter + test_counter + val_counter) % 10000 == 0:
-                        print(str(train_counter + test_counter + val_counter) + ' Completed!')
+                if patches_count == 10:
+                    patches_count = 0
+                else:
+                    patches_count += 1
+
+                if (train_count + test_count) % 10000 == 0:
+                    print(str(train_count + test_count) + ' Completed!')
+
         print('Image ' + str(img_no) + ' Completed!\n')
-        print(str(train_counter + test_counter + val_counter) + ' Completed!')
-        cv2.imwrite(out_dir + '/' + str(img_no) + '.bmp', image)
-    print('Done!')
-    print('Training Data: ' + str(train_counter))
-    print('Testing Data: ' + str(test_counter))
-    print('Validation Data: ' + str(val_counter))
+        print(str(train_count + test_count) + ' Completed!')
+        cv2.imwrite(out_dir + '/' + str(img_no) + '.bmp', original_image)
+    print('\nDone!')
+    print('Training Data: ' + str(train_count))
+    print('Testing Data: ' + str(test_count))
 
 
 def train():
-    data = Data()
+    data = Data(0.05)
     data.load_data(sys.argv[2])
     data.reduce_data(0.99)
 
     model = Model(1200, 2)
     model.set_loss_params(weights=data.get_weights())
-    accuracy, f1 = model.train(data, intervals=1, epochs=1)
-    print('Accuracy: ' + str(accuracy))
-    print('F1-Score: ' + str(f1))
+    active = Active(data, model, 2, 1.00)
+    f1_scores = active.run(5, 500, 100)
+    print(f1_scores)
 
-
+    # accuracy, f1 = model.train(data, intervals=1, epochs=1)
+    # print('Accuracy: ' + str(accuracy))
+    # print('F1-Score: ' + str(f1))
 
 
 if __name__ == '__main__':
     if sys.argv[1] == 'build':
-        try:
-            build_dataset(sys.argv[2], sys.argv[3], int(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]))
-        except:
-            print('main.py build input_dir output_dir patch_size skip test_size validation_size')
-    if sys.argv[1] == 'train':
+        build_dataset(sys.argv[2], sys.argv[3], int(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]))
+    elif sys.argv[1] == 'train':
         train()
     else:
         print('build or train')

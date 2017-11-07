3dcf7fc1b115bd76c7a1112d0e5a643f1087790e
Jacob Carse <jacob.carse@gmail.com>
2017-11-07 16:04:14 +0000

    Reworked Data class for slide images

--- Data.py
+++ Data.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import tensorflow as tf
 from collections import Counter
-from tensorflow.contrib.data import Dataset
+import tensorflow.contrib.data as tf_data
 from sklearn.model_selection import train_test_split
 
 
@@ -11,87 +11,114 @@ class Data:
     def __init__(self, val_percentage):
         self.train_x, self.train_y = [], []
         self.test_x, self.test_y = [], []
+        self.val_x, self.val_y = [], []
+        self.data_x, self.data_y = [], []
         self.val_percentage = val_percentage
 
-    def set_data(self, train_x, train_y):
-        self.train_x, self.train_y = train_x, train_y
-        self.make_val_set(self.val_percentage)
+    def load_test_data(self, test_dir):
+        for image_dir in os.listdir(test_dir):
+            if os.path.isdir(test_dir + image_dir):
+                for image_file in os.listdir(test_dir + image_dir + '/hit'):
+                    self.test_x.append(test_dir + image_dir + '/hit/' + image_file)
+                    self.test_y.append(1)
+                for image_file in os.listdir(test_dir + image_dir + '/miss'):
+                    self.test_x.append(test_dir + image_dir + '/miss/' + image_file)
+                    self.test_y.append(0)
 
-    def load_data(self, dir):
-        if dir[-1] != '/':
-            dir = dir + '/'
-        for image_file in os.listdir(dir + 'train/hit'):
-            self.train_x.append(dir + 'train/hit/' + image_file)
-            self.train_y.append(1)
-        for image_file in os.listdir(dir + 'train/miss'):
-            self.train_x.append(dir + 'train/miss/' + image_file)
-            self.train_y.append(0)
-
-        for image_file in os.listdir(dir + 'test/hit'):
-            self.test_x.append(dir + 'test/hit/' + image_file)
-            self.test_y.append(1)
-        for image_file in os.listdir(dir + 'test/miss'):
-            self.test_x.append(dir + 'test/miss/' + image_file)
-            self.test_y.append(0)
-        self.make_val_set(self.val_percentage)
+    def load_data(self, data_dir):
+        for image_dir in os.listdir(data_dir):
+            if os.path.isdir(data_dir + image_dir):
+                temp_x, temp_y = [], []
+                for image_file in os.listdir(data_dir + image_dir + '/hit'):
+                    temp_x.append(data_dir + image_dir + '/hit/' + image_file)
+                    temp_y.append(1)
+                for image_file in os.listdir(data_dir + image_dir + '/miss'):
+                    temp_x.append(data_dir + image_dir + '/miss/' + image_file)
+                    temp_y.append(0)
+                self.data_x.append(temp_x)
+                self.data_y.append(temp_y)
 
     def make_val_set(self, percentage):
         self.train_x, self.val_x, self.train_y, self.val_y = train_test_split(self.train_x, self.train_y,
                                                                               test_size=percentage)
 
-    def get_sizes(self, batch_size):
-        train_batch = int(np.floor(len(self.train_y) / batch_size))
-        test_batch = int(np.floor(len(self.test_y) / (batch_size * 100)))
-        val_batch = int(np.floor(len(self.val_y) / (batch_size * 100)))
+    def set_random_training_data(self, number):
+        for _ in range(number):
+            index = random.randint(len(self.data_y) - 1)
+            self.train_x += self.data_x[index]
+            self.train_y += self.data_y[index]
+            self.data_x.remove(index)
+            self.data_y.remove(index)
+
+    def set_training_data(self, indices):
+        if type(indices) != int:
+            for index in indices:
+                self.train_x += self.data_x[index]
+                self.train_y += self.data_y[index]
+                self.data_x.remove(index)
+                self.data_y.remove(index)
+        else:
+            self.train_x += self.data_x[indices]
+            self.train_y += self.data_y[indices]
+            self.data_x.remove(indices)
+            self.data_y.remove(indices)
+
+    def set_data(self, train_x, train_y):
+        self.train_x, self.train_y = train_x, train_y
+        self.make_val_set(self.val_percentage)
+
+    def get_num_batches(self, train_batch_size, test_batch_size):
+        train_batch = int(np.floor(len(self.train_y) / train_batch_size))
+        test_batch = int(np.floor(len(self.test_y) / (test_batch_size)))
+        val_batch = int(np.floor(len(self.val_y) / (test_batch_size)))
         return train_batch, test_batch, val_batch
 
-    def get_predict_size(self, batch_size):
-        return int(np.floor(len(self.predict_y) / batch_size))
+    def get_num_predict_batches(self, batch_size, index):
+        return int(np.floor(len(self.data_y[index]) / batch_size))
 
-    def get_datasets(self, num_threads, buffer_size, batch_size):
-        train_imgs = tf.constant(self.train_x)
+    def input_parser(self, image_path, label):
+        one_hot_label = tf.constant(tf.to_int32(label), 2)
+        image_file = tf.read_file(image_path)
+        image = tf.image.decode_image(image_file, channels=3)
+        return tf.reshape(image, [1200]), one_hot_label
+
+    def predict_input_parser(self, image_path):
+        image_file = tf.read_file(image_path)
+        image = tf.image.decode_image(image_file, channels=3)
+        return tf.reshape(image, [1200])
+
+    def get_datasets(self, num_threads, buffer_size, train_batch_size, test_batch_size):
+        train_images = tf.constant(self.train_x)
         train_labels = tf.constant(self.train_y)
-        train_dataset = Dataset.from_tensor_slices((train_imgs, train_labels))
+        train_dataset = tf_data.Dataset.from_tensor_slices((train_images, train_labels))
         train_dataset = train_dataset.map(self.input_parser, num_threads, buffer_size)
 
-        test_imgs = tf.constant(self.test_x)
+        test_images = tf.constant(self.test_x)
         test_labels = tf.constant(self.test_y)
-        test_dataset = Dataset.from_tensor_slices((test_imgs, test_labels))
+        test_dataset = tf_data.Dataset.from_tensor_slices((test_images, test_labels))
         test_dataset = test_dataset.map(self.input_parser, num_threads, buffer_size)
 
-        val_imgs = tf.constant(self.val_x)
-        val_labels = tf.constant(self.val_y)
-        val_dataset = Dataset.from_tensor_slices((val_imgs, val_labels))
-        val_dataset = val_dataset.map(self.input_parser, num_threads, buffer_size)
+        validation_images = tf.constant(self.val_x)
+        validation_labels = tf.constant(self.val_y)
+        validation_dataset = tf_data.Dataset.from_tensor_slices((validation_images, validation_labels))
+        validation_dataset = validation_dataset.map(self.input_parser, num_threads, buffer_size)
 
-        train_dataset = train_dataset.batch(batch_size)
-        test_dataset = test_dataset.batch(batch_size * 100)
-        val_dataset = val_dataset.batch(batch_size * 100)
+        train_dataset = train_dataset.batch(train_batch_size)
+        test_dataset = test_dataset.batch(test_batch_size)
+        validation_dataset = validation_dataset.batch(test_batch_size)
 
-        return train_dataset.shuffle(10000), test_dataset, val_dataset
+        return train_dataset.shuffle(10000), test_dataset, validation_dataset
 
-    def get_prediction_dataset(self, num_threads, buffer_size, batch_size):
-        imgs = tf.constant(self.predict_x)
-        predict_dataset = Dataset.from_tensor_slices((imgs))
-        predict_dataset = predict_dataset.map(self.predict_parser, num_threads, buffer_size)
+    def get_predict_dataset(self, num_threads, buffer_size, batch_size, index):
+        images = tf.constant(self.data_x[index])
+        predict_dataset = tf_data.Dataset.from_tensor_slices((images))
+        predict_dataset = predict_dataset.map(self.predict_input_parser, num_threads, buffer_size)
         return predict_dataset.batch(batch_size)
-
-    def input_parser(self, img_path, label):
-        one_hot = tf.one_hot(tf.to_int32(label), 2)
-        img_file = tf.read_file(img_path)
-        img = tf.image.decode_image(img_file, channels=3)
-        return tf.reshape(img, [1200]), one_hot
-
-    def predict_parser(self, img_path):
-        img_file = tf.read_file(img_path)
-        img = tf.image.decode_image(img_file, channels=3)
-        return tf.reshape(img, [1200])
 
     def get_weights(self):
         counter = Counter(self.train_y)
         majority = max(counter.values())
         weights = {cls: float(majority / count) for cls, count in counter.items()}
-
         nb_cl = len(weights)
         final_weights = np.ones((nb_cl, nb_cl))
         for class_idx, class_weight in weights.items():
@@ -99,37 +126,15 @@ class Data:
             final_weights[class_idx][0] = class_weight
         return final_weights
 
-    def reduce_data(self, percentage):
-        self.train_x, self.predict_x, self.train_y, self.predict_y = train_test_split(self.train_x,
-                                                                                          self.train_y,
-                                                                                          test_size=percentage)
-
     def get_bootstraps(self, num_bootstraps, bootstrap_size):
         bootstraps = []
         for _ in range(num_bootstraps):
             bootstrap_x, bootstrap_y = [], []
-            indexs = random.sample(range(len(self.train_x)), bootstrap_size)
-            for index in indexs:
+            indices = random.sample(range(len(self.train_x)), bootstrap_size)
+            for index in indices:
                 bootstrap_x.append(self.train_x[index])
                 bootstrap_y.append(self.train_y[index])
-            while bootstrap_y[1:] == bootstrap_y[:-1]:
-                index = random.randint(len(bootstrap_y) -1)
-                bootstrap_x.remove(index)
-                bootstrap_y.remove(index)
-                # TO-DO Add element to dataset.
             data = Data(self.val_percentage)
-            data.set_data(np.asarray(bootstrap_x), np.asarray(bootstrap_y))
+            data.set_data(bootstrap_x, bootstrap_y)
             bootstraps.append(data)
         return bootstraps
-
-    def increase_data(self, indexes):
-        self.train_x += self.val_x
-        self.train_y += self.val_y
-        self.val_x, self.val_y = [], []
-        for index in indexes:
-            self.train_x = np.vstack((self.train_x, [self.predict_x[index]]))
-            self.train_y = np.vstack((self.train_y, [self.predict_y[index]]))
-        self.predict_x = np.delete(self.predict_x, indexes, axis=0)
-        self.predict_y = np.delete(self.predict_y, indexes, axis=0)
-        self.make_val_set(self.val_percentage)
-

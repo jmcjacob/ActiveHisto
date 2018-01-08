5f6d5f8ffd9d6d9f38b4144f87f0fd5edeb1694e
Jacob Carse <jacob.carse@gmail.com>
2018-01-08 16:28:11 +0000

    Added active learning ranking

--- newActive.py
+++ newActive.py
@@ -1,5 +1,6 @@
 import copy
 import random
+import numpy as np
 from collections import Counter
 
 
@@ -53,16 +54,34 @@ class Active:
                 del shortlist[random.randint(0, len(shortlist)-1)]
         return shortlist
 
+    def selection(self, bootstrap_predictions, shortlist, update_size):
+        avg_predictions = []
+        for i in range(len(bootstrap_predictions[0])):
+            temp = 0
+            for j in range(len(bootstrap_predictions)):
+                for prediction in bootstrap_predictions[j][i]:
+                    temp += prediction
+            avg_predictions.append(temp / len(bootstrap_predictions[0]))
+        indices = np.array(avg_predictions).argsort()[-update_size:][::-1]
+        return_list = []
+        for index in indices:
+            return_list.append(shortlist[index])
+        return return_list
+
     def run(self, num_bootstraps, bootstrap_size, update_size):
         f1_scores = []
-        predictions, _, f1_score = self.train_predict(self.data, True, False, self.data.get_weights())
-        f1_scores.append(f1_score)
-        shortlist = self.shortlist(predictions, 500)
-
         while self.budget != self.questions:
+            predictions, _, f1_score = self.train_predict(self.data, True, False, self.data.get_weights())
+            f1_scores.append(f1_score)
+            shortlist = self.shortlist(predictions, 500)
             bootstraps = self.data.get_bootstraps(num_bootstraps, bootstrap_size, 0.2, False)
+            bootstrap_predictions = []
             for i in range(num_bootstraps):
                 print('\nBootstrap: ' + str(i))
                 bootstraps[i].reduce_data(shortlist)
                 print('Bootstrap Data Balance ' + str(Counter(bootstraps[i].train_y)))
                 predictions, _, _ = self.train_predict(bootstraps[i], False, True, bootstraps[i].get_weights())
+                bootstrap_predictions.append(predictions)
+            indices = self.selection(bootstrap_predictions, shortlist, update_size)
+            self.data.set_training_data(indices)
+            self.questions += 1
